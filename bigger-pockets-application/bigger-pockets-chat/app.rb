8c84a6b8897306a1b036e49843217296149c3bc4
Alex Bennett <alexandergraybennett@gmail.com>
2016-04-03 14:13:05 -0600

    finishing application

---
+++ bigger-pockets-application/bigger-pockets-chat/app.rb
@@ -0,0 +1,33 @@
+require 'thin'
+require 'em-websocket'
+require 'sinatra/base'
+
+EM.run do
+  class App < Sinatra::Base
+    get '/' do
+      erb :index
+    end
+  end
+
+  @clients = []
+
+  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+    ws.onopen do |handshake|
+      @clients << ws
+    end
+
+    ws.onclose do
+      ws.send "Closed."
+      @clients.delete ws
+    end
+
+    ws.onmessage do |msg|
+      puts "#{msg}"
+      @clients.each do |socket|
+        socket.send msg
+      end
+    end
+  end
+
+  App.run! :port => 3000
+end
--- chat-app/app/controllers/chat_controller.rb
+++
@@ -1,6 +0,0 @@
-class ChatController < ApplicationController
-# renders html files
-  def index
-  end
-
-end
